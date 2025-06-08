576759b2fba900ddd64b688bce9fdcf091599014
Jessica <j.jecristina@gmail.com>
2025-06-08 16:46:43 -0300

    atividade 6

---
+++ 06.js
@@ -0,0 +1,22 @@
+/**6. Memoization
+Implemente function memoize(fn) que armazene em cache chamadas
+anteriores de fn (por argumentos), retornando resultados instantâneos em
+repetidas invocações.**/
+
+const memoize = (fn) => {
+    const cache = new Map();
+    return (...args) => {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    }
+}
+
+const fib = memoize((n) => n < 2 ? n : fib(n - 1) + fib(n - 2));
+
+console.log(fib(10));
+
